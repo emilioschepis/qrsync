9bd62d85b9c9671bc1ae5ad960afb221f1c6dce4
Emilio Schepis <emilio.schepis@gmail.com>
2018-07-02 09:24:37 +0200

    Require Google Play Services
    Google Play Services availability is now checked on app start; devices without GMS can't utilise the app.

--- app/src/main/java/com/emilioschepis/qrsync/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/emilioschepis/qrsync/ui/splash/SplashActivity.kt
@@ -4,8 +4,12 @@ import android.arch.lifecycle.Observer
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import com.emilioschepis.qrsync.R
 import com.emilioschepis.qrsync.ui.codelist.CodeListActivity
 import com.emilioschepis.qrsync.ui.signin.SignInActivity
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.GoogleApiAvailability
 import org.koin.android.architecture.ext.viewModel
 
 class SplashActivity : AppCompatActivity() {
@@ -14,6 +18,14 @@ class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        if (GoogleApiAvailability.getInstance()
+                        .isGooglePlayServicesAvailable(this) != ConnectionResult.SUCCESS) {
+            Toast.makeText(this@SplashActivity,
+                    R.string.error_play_services_required, Toast.LENGTH_LONG)
+                    .show()
+            return
+        }
 
         viewModel.authentication.observe(this, Observer {
             it?.fold(this::onUserSignedOut) { onUserSignedIn() }
